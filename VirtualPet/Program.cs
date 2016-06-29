70bf9718129c0ea4900715820f3046dea2a9f3ff
Karn Sharma <karn.sharma024@gmail.com>
2016-06-28 21:15:59 -0400

    First additions

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        VirtualPet pet = new VirtualPet();
+            while (1 == 1)
+            {
+                Console.WriteLine("Your Pet Name");
+                Console.WriteLine("Hunger: " + pet.Hunger);
+                Console.WriteLine("Playfulness: " + pet.Playfulness);
+                Console.WriteLine("Energy: " + pet.Energy);
+                Console.WriteLine("");
